52feb682809936a1de242cf34c035f2e7746650c
Geonmin Park <wianw@naver.com>
2022-03-01 06:52:16 +0900

    Habit-Tracker

---
+++ src/app.jsx
@@ -0,0 +1,74 @@
+import { Component } from "react";
+import "./app.css";
+import Habits from "./components/habits";
+import Navbar from "./components/navbar";
+
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coading", count: 0 },
+    ],
+  };
+
+  handelIncrement = (habit) => {
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
+    this.setState({ habits: habits });
+  };
+  handelDecrement = (habit) => {
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
+    this.setState({ habits: habits });
+  };
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+  handleAdd = (name) => {
+    const habits = [
+      ...this.state.habits,
+      { id: Date.now(), name: name, count: 0 },
+    ];
+    this.setState({ habits });
+  };
+  handleReset = () => {
+    const habits = this.state.habits.map((item) => {
+      if (item.count !== 0) {
+        return { ...item, count: 0 };
+      }
+      return item;
+    });
+    this.setState({ habits });
+  };
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <Habits
+          key={this.state.habits.id}
+          habits={this.state.habits}
+          onIncrement={this.handelIncrement}
+          onDecrement={this.handelDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
